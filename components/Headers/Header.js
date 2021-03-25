455bb0c66e4617d97b18ef3df1ec28c4350b7840
Stephane <prevert@winch.fr>
2021-03-25 14:26:23 +0100

    Update background image

--- components/Headers/Header.js
+++ components/Headers/Header.js
@@ -5,16 +5,23 @@ import { Card, CardBody, CardTitle, Container, Row, Col } from "reactstrap";
 
 function Header() {
   return (
-    <>
-      <div className="header bg-gradient-dark pb-8 pt-5 pt-md-8">
-        <Container fluid>
-          <div className="header-body">
-            {/* Card stats */}
-            
+      <>
+          <div
+              className="header pb-8 pt-5 pt-lg-8 d-flex align-items-center"
+              style={{
+                  minHeight: "300px",
+                  backgroundImage:
+                      "url(" + require("assets/img/theme/old-books-436498.jpg") + ")",
+                  backgroundSize: "cover",
+                  backgroundPosition: "center top",
+              }}
+          >
+              {/* Mask */}
+              <span className="mask bg-gradient-red opacity-8" />
+              {/* Header container */}
+
           </div>
-        </Container>
-      </div>
-    </>
+      </>
   );
 }
 
