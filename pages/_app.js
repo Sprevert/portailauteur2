8ddcf5a529ec03fbc1a829d9ac3573c74a7be9b5
Stephane <prevert@winch.fr>
2021-03-25 15:15:02 +0100

    Portail Auteurs

--- pages/_app.js
+++ pages/_app.js
@@ -54,7 +54,7 @@ export default class MyApp extends App {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
-          <title>Portail Auteur</title>
+          <title>Portail Auteurs</title>
           <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
         </Head>
         <Layout>
